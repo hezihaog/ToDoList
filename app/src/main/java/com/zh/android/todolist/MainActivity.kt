11ca10f1e15ab8530c58eddd340da49d45963a24
zihe <he100zihao100@126.com>
2020-06-22 20:51:44 +0800

    fix: 优化WebView。

--- app/src/main/java/com/zh/android/todolist/MainActivity.kt
+++ app/src/main/java/com/zh/android/todolist/MainActivity.kt
@@ -3,11 +3,25 @@ package com.zh.android.todolist
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.view.ViewGroup
 import android.webkit.WebSettings
 import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private val vWebView by lazy {
+        val mainContent = findViewById<ViewGroup>(R.id.main_content)
+        val webView = WebView(this)
+        mainContent.addView(
+            webView,
+            ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+            )
+        )
+        webView
+    }
+
     /**
      * Url地址
      */
@@ -17,50 +31,61 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val webView = findViewById<WebView>(R.id.web_view)
-        webView.apply {
+        vWebView.apply {
             //远程支持Debug
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
                 WebView.setWebContentsDebuggingEnabled(true)
             }
-            val settings: WebSettings = settings
-            //当页面宽度大于WebView宽度时，缩小使页面宽度等于WebView宽度
-            settings.loadWithOverviewMode = false
-            //支持JS
-            settings.javaScriptEnabled = true
-            //支持通过JS打开新窗口
-            settings.javaScriptCanOpenWindowsAutomatically = true
-            //将图片调整到合适WebView的大小
-            settings.useWideViewPort = true
-            //缩放至屏幕的大小
-            settings.loadWithOverviewMode = true
-            //支持自动加载图片
-            settings.loadsImagesAutomatically = true
-            //支持多窗口
-            settings.supportMultipleWindows()
-            //支持缩放
-            //settings.setSupportZoom(true);
-            settings.setSupportZoom(false)
-            //支持双击缩放、缩小
-            //settings.setBuiltInZoomControls(true);
-            settings.builtInZoomControls = false
-            //设置允许访问见
-            settings.allowFileAccess = true
-            //设置支持访问内容，支持file协议
-            settings.allowContentAccess = true
-            //设置是否允许通过 file url 加载的 Js代码读取其他的本地文件
-            settings.allowFileAccessFromFileURLs = true
-            //设置是否允许通过 file url 加载的 Javascript 可以访问其他的源(包括http、https等源)
-            settings.allowUniversalAccessFromFileURLs = true
-            //开启DOM存储
-            settings.domStorageEnabled = true
-            //不进行缓存
-            settings.cacheMode = WebSettings.LOAD_NO_CACHE
-            //5.0以上允许加载http和https混合的页面(5.0以下默认允许，5.0+默认禁止)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            settings.apply {
+                //当页面宽度大于WebView宽度时，缩小使页面宽度等于WebView宽度
+                loadWithOverviewMode = false
+                //支持JS
+                javaScriptEnabled = true
+                //支持通过JS打开新窗口
+                javaScriptCanOpenWindowsAutomatically = true
+                //将图片调整到合适WebView的大小
+                useWideViewPort = true
+                //缩放至屏幕的大小
+                loadWithOverviewMode = true
+                //支持自动加载图片
+                loadsImagesAutomatically = true
+                //支持多窗口
+                supportMultipleWindows()
+                //支持缩放
+                //settings.setSupportZoom(true);
+                setSupportZoom(false)
+                //支持双击缩放、缩小
+                //settings.setBuiltInZoomControls(true);
+                builtInZoomControls = false
+                //设置允许访问见
+                allowFileAccess = true
+                //设置支持访问内容，支持file协议
+                allowContentAccess = true
+                //设置是否允许通过 file url 加载的 Js代码读取其他的本地文件
+                allowFileAccessFromFileURLs = true
+                //设置是否允许通过 file url 加载的 Javascript 可以访问其他的源(包括http、https等源)
+                allowUniversalAccessFromFileURLs = true
+                //开启DOM存储
+                domStorageEnabled = true
+                //不进行缓存
+                cacheMode = WebSettings.LOAD_NO_CACHE
+                //5.0以上允许加载http和https混合的页面(5.0以下默认允许，5.0+默认禁止)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+                }
             }
+            loadUrl(mWebUrl)
         }
-        webView.loadUrl(mWebUrl)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        vWebView.apply {
+            parent?.also {
+                (it as ViewGroup).removeView(vWebView)
+            }
+            removeAllViews()
+            destroy()
+        }
     }
 }
